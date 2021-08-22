532e57de7eeddba97b9f6849a628270ffbc64ef5
Jay Goyal <jay@arch-desktop.localdomain>
2021-08-22 22:25:51 +0530

    Switched to qutebrowser

--- arch_scripts/install.sh
+++ arch_scripts/install.sh
@@ -1,9 +1,10 @@
-su jay
 cd ~
-sudo pacman -S rustup
+sudo pacman -S rustup base-devel --needed
 rustup install stable
 git clone https://aur.archlinux.org/paru-git.git ~/paru
 cd paru
 makepkg -si
-SOFTWARE="`sed -e 's/#.*$//' -e '/^$/d' software.txt | tr '\n' ' '`"
-paru -S $SOFTWARE
+SOFTWARE="`sed -e 's/#.*$//' -e '/^$/d' ~/dotfiles/arch_scripts/software.txt | tr '\n' ' '`"
+paru -S $SOFTWARE --needed
+sudo systemctl enable NetworkManager
+xdg-user-dirs-update
