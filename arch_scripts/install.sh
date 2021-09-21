058db347cb3d17395859117d98a53002218d4f41
Jay Goyal <goyal.jay2003@gmail.com>
2021-09-21 18:55:57 +0530

    Migrated to VS Code

--- arch_scripts/install.sh
+++ arch_scripts/install.sh
@@ -5,6 +5,13 @@ git clone https://aur.archlinux.org/paru-git.git ~/paru
 cd paru
 makepkg -si
 software="`sed -e 's/#.*$//' -e '/^$/d' ~/dotfiles/arch_scripts/software.txt | tr '\n' ' '`"
+ext="`sed -e 's/#.*$//' -e '/^$/d' ~/dotfiles/arch_scripts/ext.txt | tr '\n' ' '`"
 paru -S $software --needed
 sudo systemctl enable NetworkManager
+cp -r ~/dotfiles/.config ~/
+cp -r ~/dotfiles/.xinitrc ~/
+cp -r ~/dotfiles/.zprofile ~/
+cp -r ~/dotfiles/.zshenv ~/
+cp -r ~/dotfiles/.zshrc ~/
+cp -r ~/dotfiles/wallpapers ~/
 xdg-user-dirs-update
