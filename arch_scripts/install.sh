0a959463fad308d2c67e965cf8a5d6c1475c7805
Jay Goyal <goyal.jay2003@gmail.com>
2021-09-21 19:20:50 +0530

    Automate extensions installation procedure

--- arch_scripts/install.sh
+++ arch_scripts/install.sh
@@ -5,8 +5,20 @@ git clone https://aur.archlinux.org/paru-git.git ~/paru
 cd paru
 makepkg -si
 software="`sed -e 's/#.*$//' -e '/^$/d' ~/dotfiles/arch_scripts/software.txt | tr '\n' ' '`"
-ext="`sed -e 's/#.*$//' -e '/^$/d' ~/dotfiles/arch_scripts/ext.txt | tr '\n' ' '`"
+ext=(
+acestojanoski.ace-palenight
+esbenp.prettier-vscode
+matklad.rust-analyzer
+PKief.material-icon-theme
+PKief.material-product-icons
+ritwickdey.LiveServer
+vscodevim.vim
+)
 paru -S $software --needed
+for ex in $ext
+do
+code --install-extension $ex
+done
 sudo systemctl enable NetworkManager
 cp -r ~/dotfiles/.config ~/
 cp -r ~/dotfiles/.xinitrc ~/
