864267e2c369dd87357314f2be53c20b69479e8b
Jay Goyal <goyal.jay2003@gmail.com>
2021-09-22 12:18:45 +0530

    Added emacs back... I know

--- arch_scripts/install.sh
+++ arch_scripts/install.sh
@@ -2,6 +2,8 @@ cd ~
 sudo pacman -S rustup base-devel --needed
 rustup install stable
 git clone https://aur.archlinux.org/paru-git.git ~/paru
+git clone https://github.com/plexus/chemacs2.git ~/.emacs.d
+git clone --depth 1 https://github.com/hlissner/doom-emacs ~/.doom-emacs
 cd paru
 makepkg -si
 software="`sed -e 's/#.*$//' -e '/^$/d' ~/dotfiles/arch_scripts/software.txt | tr '\n' ' '`"
@@ -21,9 +23,10 @@ code --install-extension $ex
 done
 sudo systemctl enable NetworkManager
 cp -r ~/dotfiles/.config ~/
-cp -r ~/dotfiles/.xinitrc ~/
-cp -r ~/dotfiles/.zprofile ~/
-cp -r ~/dotfiles/.zshenv ~/
-cp -r ~/dotfiles/.zshrc ~/
 cp -r ~/dotfiles/wallpapers ~/
+cp -r ~/dotfiles/.emacs.d ~/
+cp ~/dotfiles/.xinitrc ~/
+cp ~/dotfiles/.zprofile ~/
+cp ~/dotfiles/.zshenv ~/
+cp ~/dotfiles/.zshrc ~/
 xdg-user-dirs-update
