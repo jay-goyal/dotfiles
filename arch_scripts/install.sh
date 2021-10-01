e976e7201c7689c5aaaf68937a060cd3459c3a69
Jay Goyal <goyal.jay2003@gmail.com>
2021-10-01 09:25:47 +0000

    Update install.sh

--- arch_scripts/install.sh
+++ arch_scripts/install.sh
@@ -2,8 +2,6 @@ cd ~
 sudo pacman -S rustup base-devel --needed
 rustup install stable
 git clone https://aur.archlinux.org/paru-git.git ~/paru
-git clone https://github.com/plexus/chemacs2.git ~/.emacs.d
-git clone --depth 1 https://github.com/hlissner/doom-emacs ~/.doom-emacs
 cd paru
 makepkg -si
 software="`sed -e 's/#.*$//' -e '/^$/d' ~/dotfiles/arch_scripts/software.txt | tr '\n' ' '`"
