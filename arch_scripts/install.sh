47364e39cb2764ca603bd152ab15eaa6b8544ad1
Jay Goyal <goyal.jay2003@gmail.com>
2021-10-01 09:33:41 +0000

    Update install.sh

--- arch_scripts/install.sh
+++ arch_scripts/install.sh
@@ -7,6 +7,7 @@ makepkg -si
 software="`sed -e 's/#.*$//' -e '/^$/d' ~/dotfiles/arch_scripts/software.txt | tr '\n' ' '`"
 paru -S $software --needed
 sudo systemctl enable NetworkManager
+sudo systemctl enable lightdm
 cp -r ~/dotfiles/.config ~/
 cp -r ~/dotfiles/wallpapers ~/
 cp -r ~/dotfiles/.emacs.d ~/
