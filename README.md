153bb9ebd60eef07bf2370c36dacea4176967734
Jay Goyal <goyal.jay2003@gmail.com>
2021-10-25 22:04:35 +0530

    Switched from gitlab to github

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # Dotfiles
 My Personal Dotfiles...
-Alacritty, BSPWM, Rofi, Emacs, NeoVim, SXHKD, Qutebrowser and other stuff
+Alacritty, BSPWM, Emacs, NeoVim, SXHKD and other stuff
--- arch_scripts/install.sh
+++ arch_scripts/install.sh
@@ -4,7 +4,7 @@ rustup install stable
 git clone https://aur.archlinux.org/paru-git.git ~/paru
 cd ~/paru
 makepkg -si
-git clone https://gitlab.com/jgoyal/super-dmenu.git ~/super_dmenu
+git clone https://github.com/jgoyal/super-dmenu.git ~/super_dmenu
 cd ~/super_dmenu
 sudo make install
 software="`sed -e 's/#.*$//' -e '/^$/d' ~/dotfiles/arch_scripts/software.txt | tr '\n' ' '`"
