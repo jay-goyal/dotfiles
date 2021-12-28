e55ed34a7ef7e48f2fcf80a2167556db9dfcf431
Jay Goyal <jay@jay-g14.localdomain>
2021-12-28 19:12:39 +0530

    Changed the install scripts for the G14

--- arch_scripts/install.sh
+++ arch_scripts/install.sh
@@ -4,12 +4,6 @@ rustup install stable
 git clone https://aur.archlinux.org/paru-git.git ~/paru
 cd ~/paru
 makepkg -si
-git clone https://github.com/jay-goyal/super-dmenu.git ~/super-dmenu
-cd ~/super-dmenu
-sudo make clean install
-git clone https://github.com/jay-goyal/super-dwm.git ~/super-dwm
-cd ~/super-dwm
-sudo make clean install
 software="`sed -e 's/#.*$//' -e '/^$/d' ~/dotfiles/arch_scripts/software.txt | tr '\n' ' '`"
 yarn_soft="`sed -e 's/#.*$//' -e '/^$/d' ~/dotfiles/arch_scripts/yarn-pkg.txt | tr '\n' ' '`"
 paru -S $software --needed
@@ -18,6 +12,16 @@ yarn global add $yarn_soft
 sudo systemctl enable NetworkManager
 sudo systemctl enable lightdm
 sudo systemctl enable libvirtd
+sudo systemctl enable power-profiles-daemon.service
+sudo systemctl enable supergfxd
+sudo systemctl enable auto-cpufreq
+grub-mkconfig -o /boot/grub.cfg
+git clone https://github.com/jay-goyal/super_dmenu.git ~/super-dmenu
+cd ~/super-dmenu
+sudo make clean install
+git clone https://github.com/jay-goyal/super-dwm.git ~/super-dwm
+cd ~/super-dwm
+sudo make clean install
 cp -r ~/dotfiles/.config ~/
 cp -r ~/dotfiles/.dwm ~/
 cp -r ~/dotfiles/wallpapers ~/
