5514887dcec18bd38cc90fb1f251978ae111c6fb
Jay Goyal <jay@jay-g14.localdomain>
2022-01-05 14:31:06 +0530

    BITS Net Auth

--- .dwm/autostart.sh
+++ .dwm/autostart.sh
@@ -2,6 +2,7 @@ killall emacs
 killall polybar
 picom &
 $HOME/.config/polybar/launch-dwm.sh &
+$HOME/BITSNETAuth/bin/caa --daemon &
 nm-applet &
 feh --randomize --bg-fill ~/wallpapers/* &
 thunar --daemon &
