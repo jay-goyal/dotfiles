081c3749123205f7838384121f125f93c0dc4220
Jay Goyal <jay@jay-g14.localdomain>
2022-01-10 11:51:28 +0530

    Made caa have a delayed start

--- .dwm/autostart.sh
+++ .dwm/autostart.sh
@@ -2,9 +2,10 @@ killall emacs
 killall polybar
 picom &
 $HOME/.config/polybar/launch-dwm.sh &
-$HOME/BITSNETAuth/bin/caa --daemon &
 nm-applet &
 feh --randomize --bg-fill ~/wallpapers/* &
 thunar --daemon &
 emacs --daemon &
 lxsession &
+killall caa
+sleep 3 && $HOME/BITSNetAuth/bin/caa --daemon &
