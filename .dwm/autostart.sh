b1358f0c1a4f6e6a2db485de760b6a10cb5e76f1
Jay Goyal <goyal.jay2003@gmail.com>
2022-01-26 10:00:57 +0530

    Changes to dwm autostart

--- .dwm/autostart.sh
+++ .dwm/autostart.sh
@@ -1,12 +1,13 @@
 killall emacs
 killall polybar
+killall dunst
 picom &
 $HOME/.config/polybar/launch-dwm.sh &
 nm-applet &
 feh --randomize --bg-fill ~/wallpapers/* &
 thunar --daemon &
-emacs --daemon &
 lxsession &
 dunst &
-killall caa
-sleep 3 && $HOME/BITSNetAuth/bin/caa --daemon &
+emacs --daemon &
+#killall caa
+#sleep 3 && $HOME/BITSNetAuth/bin/caa --daemon &
