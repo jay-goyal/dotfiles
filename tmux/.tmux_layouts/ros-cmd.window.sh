2ab4c3c857853238cc389a0da1665dec5388a04b
Jay Goyal <goyal.jay2003@gmail.com>
2025-04-06 13:33:29 +0530

    Remove TMUX Layouts

--- tmux/.tmux_layouts/ros-cmd.window.sh
+++
@@ -1,3 +0,0 @@
-new_window "ros-cmd"
-run_cmd "distrobox enter ubuntu-ros-noetic"
-run_cmd "source ./devel/setup.zsh"
--- tmux/.tmux_layouts/ros2-cmd.window.sh
+++
@@ -1,6 +0,0 @@
-new_window "ros2-cmd"
-run_cmd "distrobox enter ubuntu-ros-humble"
-run_cmd "source /opt/ros/humble/setup.zsh"
-run_cmd "source ./install/setup.zsh"
-run_cmd 'eval "$(register-python-argcomplete3 ros2)"'
-run_cmd 'eval "$(register-python-argcomplete3 colcon)"'
