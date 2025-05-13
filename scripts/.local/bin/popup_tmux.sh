22adc878676c633ef0607bc54550c0366dba6589
Jay Goyal <goyal.jay2003@gmail.com>
2025-05-13 22:46:25 +0530

    Update tmux popup script

--- scripts/.local/bin/popup_tmux.sh
+++ scripts/.local/bin/popup_tmux.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 
-if [ "$(tmux display-message -p -F "#{session_name}")" = "popup" ]; then
+if [[ "$(tmux display-message -p -F '#{session_name}')" == *popup ]]; then
 	tmux detach-client
 else
-	tmux popup -b rounded -E -w 80% -h 80% "tmux -u attach -t popup || tmux -u new -s popup"
+	# Get the current session name
+	current_session=$(tmux display-message -p -F "#{session_name}")
+	# Create a new session name by appending "popup" to the current session name
+	popup_session="${current_session}-popup"
+
+	# Create the popup with the new session name
+	tmux popup -b rounded -E -w 80% -h 80% "tmux -u attach -t $popup_session || tmux -u new -s $popup_session"
 fi
