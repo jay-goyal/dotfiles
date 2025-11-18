19120893c3d246bd366efa59e78f66675b1cdd88
Jay Goyal <goyal.jay2003@gmail.com>
2025-11-18 22:09:34 +0530

    add undotree

--- nvim/.config/nvim/lua/plugins/misc.lua
+++ nvim/.config/nvim/lua/plugins/misc.lua
@@ -11,6 +11,17 @@ return {
             line_num = { enable = true, style = "#FF9E64" },
         },
     },
+
+    -- Undotree
+    {
+        "jiaoshijie/undotree",
+        opts = {
+            window = { border = "single" },
+        },
+        keys = { -- load the plugin only when using it's keybinding:
+            { "<leader>u", "<cmd>lua require('undotree').toggle()<cr>" },
+        },
+    },
     -- Misc
     "tpope/vim-sleuth",
     {
